4ee076dc01ed420a43c0a6a513cc1f7ac8c07169
gagankaul <63837949+gagankaul@users.noreply.github.com>
2022-02-15 20:01:58 -0500

    Enter

--- lettercounterapp.py
+++ lettercounterapp.py
@@ -2,20 +2,17 @@
 
 print("Welcome to the Letter Counter App\n")
 
-name = input("What is your name: ")
-
-print("Hello, " + name.title() + "!" + "\n")
-
+# User inputs section
+name = input("What is your name: ").title().strip()
+print("Hello, " + name + "!")
 print("I will count the number of times a specific letter appears in a message.")
-
 message = input("\nPlease enter a message: ")
-
-message = message.lower()
-
 letter = input("Which letter would you like to count the occurences for: ")
 
+# Standardize to lower case
+message = message.lower()
 letter = letter.lower()
 
+# Get the count and display results
 letter_count = message.count(letter)
-
-print(name.title() + ", your message has " + str(letter_count) + " " + letter + "'s" + " in it.")
+print("\n" + name.title() + ", your message has " + str(letter_count) + " " + letter + "'s in it.")
